31894b5dddda75986bca77ecd84afaa0273e360f
Roman Sorokowski <xardas4400@gmail.com>
2014-01-25 10:39:06 +0100

    porzadek w Mainie

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,7 @@
 package main;
 
-import io.csv.CSVWriter;
+import io.clarin.ClarinWsdWorker;
+import io.clarin.ClarinXmlParser;
 import io.document.DocumentLoader;
 import io.documentmap.ObjectIOManager;
 
@@ -20,89 +21,97 @@ import documentmap.document.Document;
 
 public class Main {
 
-	private static final String DIR = "C:/Users/XardaS/Desktop";
-	private static final String KPWR = "kpwr_wsd_blogi";
-	private static final String CSV = "csv_blogi";
-	private static final String DICT = "dict_blogi.csv";
-
 	public static void main(String[] args) throws Exception {
-		// final JFrame frame = new JFrame();
-		// frame.setSize(new Dimension(800, 600));
-		// frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		// DocumentMap map = new DocumentMap(1, 10, 5);
-		// GraphPanel panel = new GraphPanel(map);
-		// frame.setContentPane(panel);
-		// SwingUtilities.invokeLater(new Runnable() {
-		//
-		// @Override
-		// public void run() {
-		// frame.setVisible(true);
-		// }
-		// });
-		// DocumentLoader loader = new DocumentLoader(new
-		// File("D:\\Studia\\IJN\\kpwr-1.1"), new File("D:\\Studia\\IJN\\csv"));
-		// System.out.println(loader.loadDocuments(10, true, false));
-
-//		 ClarinWsdWorker w = new ClarinWsdWorker("C:\\Users\\XardaS\\Desktop\\kpwr");
-//		 w.disambiguateCorpus(false);
-
-		// ClarinXmlParser p = new ClarinXmlParser(KPWR, CSV);
-		// p.parse(false);
-		// p.saveDictionary("C:\\Users\\XardaS\\Desktop\\dick.csv");
-//		learnSom();
-		test();
+		if (args.length > 0) {
+			String task = args[0];
+			if (task.equals("DISAMB")) {
+				String corpusPath = args[1];
+				String text = args[2];
+				disambiguate(corpusPath, text.equals("XML"));
+			} else if (task.equals("EXTRACT")) {
+				String corpusPath = args[1];
+				String outputPath = args[2];
+				String synsets = args[3];
+				String dictionaryPath = args[4];
+				extract(corpusPath, outputPath, synsets.equals("SYNSETS"), dictionaryPath);
+			} else if (task.equals("LEARN")) {
+				String type = args[1];
+				if (type.equals("BAYES")) {
+					String directory = args[2];
+					String corpusPath = args[3];
+					String csvPath = args[4];
+					String savePath = args[5];
+					learnBayes(directory, corpusPath, csvPath, savePath);
+				} else if (type.equals("SOM")) {
+					String corpusPath = args[2];
+					String csvPath = args[3];
+					String dictionaryPath = args[4];
+					String savePath = args[5];
+					String mapX = args[6];
+					String mapY = args[7];
+					int[] mapDim = new int[] { Integer.valueOf(mapX), Integer.valueOf(mapY) };
+					learnSom(corpusPath, csvPath, dictionaryPath, savePath, mapDim);
+				}
+			} else if (task.equals("VISUALIZE")) {
+				String mapPath = args[2];
+				String corpusPath = args[3];
+				String csvPath = args[4];
+				String dictionaryPath = args[5];
+				visualize(mapPath, corpusPath, csvPath, dictionaryPath);
+			}
+		}
 	}
-	
-	public static void learnSom() throws IOException{
-		DocumentLoader loader = new DocumentLoader(new File(KPWR), new File(CSV));
+
+	private static void disambiguate(String corpusPath, boolean text) throws Exception {
+		ClarinWsdWorker worker = new ClarinWsdWorker(corpusPath);
+		worker.disambiguateCorpus(text);
+	}
+
+	private static void extract(String corpusPath, String outputPath, boolean synsets, String dictionaryPath) throws Exception {
+		ClarinXmlParser parser = new ClarinXmlParser(corpusPath, outputPath);
+		parser.parse(synsets);
+		parser.saveDictionary(dictionaryPath);
+	}
+
+	public static void learnSom(String corpusPath, String csvPath, String dictionaryPath, String savePath, int[] mapDim) throws IOException {
+		DocumentLoader loader = new DocumentLoader(new File(corpusPath), new File(csvPath));
 		List<Document> documents = loader.loadDocuments();
-		double[][] vectors =  Document.vectorizeDocuments(documents, new File(DICT));
-		DocumentMap map = new DocumentMap(vectors[0].length, 50, 50);
+		double[][] vectors = Document.vectorizeDocuments(documents, new File(dictionaryPath));
+		DocumentMap map = new DocumentMap(vectors[0].length, mapDim[0], mapDim[1]);
 		map.createMap(vectors);
-		ObjectIOManager.save(map, new File("rawSynset.map"));
+		ObjectIOManager.save(map, new File(savePath));
 	}
 
-	public static void test() throws Exception {
-//		DocumentMap map = new DocumentMap(12, 50, 50);
-//		List<String> args = new LinkedList<String>();
-//		args.add("LEARN_AND_CLASSIFY");
-//		args.add(DIR + "\\learn.csv");
-//		args.add("s");
-//		args.add("MULTINOMINAL");
-//		args.add("BY_WORD");
-//		args.add("SIMPLE_SMOOTHING");
-//		args.add(DIR + "\\test.csv");
-//		BayesParams params = new BayesParams(args.toArray(new String[args.size()]));
-//		DocumentLoader loader = new DocumentLoader(new File(KPWR), new File(CSV));
-//		List<String> categories = loader.getCategoryList();
-//		List<Document> learning = new LinkedList<Document>();
-//		List<Document> test = new LinkedList<Document>();
-//		List<Document> test2 = new LinkedList<Document>();
-//		for (String cat : categories) {
-//			learning.addAll(loader.loadDocuments(cat, 5, true));
-//			test.addAll(loader.loadDocuments(cat, 5, true));
-//			test2.addAll(loader.loadDocuments(cat, 10, true));
-//		}
-//		map.createMap(params, test, learning, categories);
-//		ObjectIOManager.save(map, new File("temp.map"));
-//		args.clear();
-//		args.add("CLASSIFY_ONLY");
-//		args.add(DIR + "\\learnLEARNED.csv");
-//		args.add("MULTINOMINAL");
-//		args.add(DIR + "\\test2.csv");
-//		params = new BayesParams(args.toArray(new String[args.size()]));
-		
-		
-		DocumentMap map = ObjectIOManager.load(new File("rawSynsetBlogi2.map"));
-//		
-		DocumentLoader loader = new DocumentLoader(new File(KPWR), new File(CSV));
-//		List<Document> docs =loader.loadDocuments();
-//		
-//		DocumentMap.createVectors(new File("synset.pca"), new File(DICT), docs);
-		
+	private static void learnBayes(String directory, String corpusPath, String csvPath, String mapOutputPath) throws Exception {
+		DocumentMap map = new DocumentMap(12, 50, 50);
+		List<String> args = new LinkedList<String>();
+		args.add("LEARN_AND_CLASSIFY");
+		args.add(directory + "\\learn.csv");
+		args.add("s");
+		args.add("MULTINOMINAL");
+		args.add("BY_WORD");
+		args.add("SIMPLE_SMOOTHING");
+		args.add(directory + "\\test.csv");
+		BayesParams params = new BayesParams(args.toArray(new String[args.size()]));
+		DocumentLoader loader = new DocumentLoader(new File(corpusPath), new File(csvPath));
+		List<String> categories = loader.getCategoryList();
+		List<Document> learning = new LinkedList<Document>();
+		List<Document> test = new LinkedList<Document>();
+		List<Document> test2 = new LinkedList<Document>();
+		for (String cat : categories) {
+			learning.addAll(loader.loadDocuments(cat, 5, true));
+			test.addAll(loader.loadDocuments(cat, 5, true));
+			test2.addAll(loader.loadDocuments(cat, 10, true));
+		}
+		map.createMap(params, test, learning, categories);
+		ObjectIOManager.save(map, new File(mapOutputPath));
+	}
+
+	public static void visualize(String mapPath, String corpusPath, String csvPath, String dictionaryPath) throws Exception {
+		DocumentMap map = ObjectIOManager.load(new File(mapPath));
+		DocumentLoader loader = new DocumentLoader(new File(corpusPath), new File(csvPath));
 		List<Document> documents = loader.loadDocuments();
-		List<int[]> list = map.mapDocuments(documents, new File(DICT));
-		
+		List<int[]> list = map.mapDocuments(documents, new File(dictionaryPath));
 		final JFrame frame = new JFrame();
 		frame.setSize(new Dimension(800, 600));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
